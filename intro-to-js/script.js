440fda958c54f72ed16d67b3a6dc392eeeb9a349
ishajazmine <a.fredericksen@live.com.au>
2022-01-22 10:33:22 +0800

    work to date aa 220122

--- intro-to-js/script.js
+++ intro-to-js/script.js
@@ -1,20 +1,36 @@
 window.onload = function() {
     // start
+    let starterDiv = document.getElementById('starter-div');
+    let firstHeading = document.createElement('h2');
+    firstHeading.innerText = 'This is an H2 created with JS';
+    firstHeading.classList.add('js-header');
 
+    starterDiv.appendChild(firstHeading);
+// append headings under the relevant code for readability.
+
+    let secondHeading = document.createElement('h2');
+    secondHeading.innerText = 'This is another H2 element created with JS. Help meeee'
+    secondHeading.classList.add('js-header');
+    secondHeading.id = 'second-h2';
+
+    starterDiv.appendChild(secondHeading);
     // end
 }
 
 
 function showName(){
     console.log('showName');
+    // ^can be viewed on inspect > console> to make sure function is working
     // start
+    let name = document.getElementById('name').value;
 
+    document.getElementById('greeting-text').innerText = 'Hi ' + name;
     // end
 }
 
 function changeColour(colour) {
     // start
-
+    document.body.style.backgroundColor = colour;
     // end
 }
 
@@ -24,6 +40,11 @@ function resetColour() {
 
 function toggleLightbulb() {
     // start
+    let lightbulb = document.getElementById('light');
+    lightbulb.classList.toggle("on");
+    // ^^to toggle whenever button is clicked
+    console.log(lightbulb);
+    // ^everytime you click will show in console log.
 
     // end
 }
--- keyboard/script.js
+++ keyboard/script.js
@@ -1,23 +1,52 @@
 window.onload = function() {
     let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
     // start
+    let keyboard = document.getElementById('keyboard');
+
 
     // end
 
     alphabet.forEach(letter => {
         // start
-
+        let keyButton = document.createElement('input');
+        keyButton.type = 'button';
+        keyButton.value = letter;
+        keyButton.addEventListener('click', event => {
+            console.log(event)
+            highlightLetter(event.target.id)
+        })
+        keyButton.classList.add('key');
+        keyButton.id = letter;
+        keyboard.appendChild(keyButton);
         // end
     });
 }
 
 // start
+document.addEventListener('keydown', event => {
+    highlightLetter(event.key)
+})
+// ^^^^ connects it to keyboard. 
 
 // end
 
 
 function highlightLetter(letter){
     // start
+    console.log(letter)
+    let output = document.getElementById('output-text');
+    output.innerText = output.innerText + letter;
+
+    // ^^displays letter
+    let key = document.getElementById(letter);
+    key.classList.toggle('selected');
+
+    // creating a toggle 
+    setTimeout(function() {
+        key.classList.toggle('selected');
+    }, 150);
+    // when doing toggle. after x milliseconds we want to turn off the list - so briefly highlights
 
     // end
 }
+
